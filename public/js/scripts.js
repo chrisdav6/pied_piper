e15f26733e42bda80fe3f90845601fdab1a7e780
Chris Davis <chris.davis5440@gmail.com>
2018-02-21 19:29:06 +0000

    Pied Piper Mockup Site first Commit

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+  
+  //Smooth Scroll
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+          scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+  
+});
+
